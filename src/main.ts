a2b63d0530238a16f8a11e754762c70f4734f9b9
woojiahao <woojiahao1234@gmail.com>
2021-10-08 09:17:11 +0800

    Add Typescript and React to project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+
+
+app.on('ready', () => {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+})
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackConfigPlugin = require('html-webpack-plugin')
+
+module.exports = [{
+    mode: 'development',
+    entry: './src/main.ts',
+    target: 'electron-main',
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        include: /src/,
+        use: [{
+          loader: 'ts-loader'
+        }]
+      }]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'main.js'
+    }
+  },
+  {
+    mode: 'development',
+    entry: './src/react.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    module: {
+      rules: [{
+        test: /\.ts(x?)$/,
+        include: /src/,
+        use: [{
+          loader: 'ts-loader'
+        }]
+      }]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'react.js'
+    },
+    plugins: [
+      new HtmlWebpackConfigPlugin({
+        template: './src/index.html'
+      })
+    ]
+  }
+]
