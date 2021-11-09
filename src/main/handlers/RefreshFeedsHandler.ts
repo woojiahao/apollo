cdbfe7bd152bae4cef9a9b26bced297ea5deb057
woojiahao <woojiahao1234@gmail.com>
2021-11-09 13:46:02 +0800

    Setup RefreshFeedsHandler to conver to TagFeeds

--- src/main/handlers/RefreshFeedsHandler.ts
+++ src/main/handlers/RefreshFeedsHandler.ts
@@ -1,14 +1,18 @@
 import { getCustomRepository } from "typeorm";
 import Feed from "../database/entities/Feed";
+import FeedMapper from "../database/mappers/FeedMapper";
 import FeedRepository from "../database/repositories/FeedRepository";
+import { RSS } from "../rss/data";
 import Handler from "./Handler";
 import { refreshFeed } from "./utility";
 
-export default class RefreshFeedsHandler implements Handler<Feed[]> {
+export default class RefreshFeedsHandler implements Handler<RSS.TagFeeds> {
   async handle() {
     const feedRepository = getCustomRepository(FeedRepository)
     const availableFeeds = await feedRepository.getAvailable()
     const updatedFeeds = await Promise.all(availableFeeds.map(refreshFeed))
-    return feedRepository.save(updatedFeeds)
+    const savedFeeds = await feedRepository.save(updatedFeeds)
+    const tagFeeds = FeedMapper.toTagFeeds(savedFeeds)
+    return tagFeeds
   }
 }
