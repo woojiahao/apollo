3da1ad65e33d7951b76653d871ce7e17f2485238
woojiahao <woojiahao1234@gmail.com>
2021-11-18 16:05:47 +0800

    Order articles of feed by isRead and publishedDate

--- src/main/database/repositories/FeedRepository.ts
+++ src/main/database/repositories/FeedRepository.ts
@@ -3,11 +3,16 @@ import Feed from "../entities/Feed";
 
 @EntityRepository(Feed)
 export default class FeedRepository extends Repository<Feed> {
+  /// Retrieves the available articles, prioritizing unread articles first
   getAvailable(): Promise<Feed[]> {
-    const availableFeeds = this.find({
-      where: { deletedOn: IsNull() },
-      relations: ['tag', 'articles']
-    })
+    const availableFeeds = this
+      .createQueryBuilder('feed')
+      .innerJoinAndSelect('feed.articles', 'article')
+      .innerJoinAndSelect('feed.tag', 'tag')
+      .where('feed.deletedOn is null')
+      .orderBy('article.isRead')
+      .addOrderBy('article.publishedDate')
+      .getMany()
     return availableFeeds
   }
 
