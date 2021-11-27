7bdda03cd74cb5d0eeacf08a6eb01d2dc60939a5
woojiahao <woojiahao1234@gmail.com>
2021-11-28 01:08:44 +0800

    Set StyledTextField to forward a ref

--- src/renderer/components/form/StyledTextField.tsx
+++ src/renderer/components/form/StyledTextField.tsx
@@ -5,13 +5,13 @@ interface StyledTextFieldProps {
   label: string
 }
 
-export default class StyledTextField extends React.Component<StyledTextFieldProps> {
-  render() {
-    return (
-      <div className="flex flex-col">
-        <label className="block mb-2 text-subtitle font-bold" htmlFor={this.props.label}>{this.props.title}</label>
-        <input type="text" id={this.props.label} name={this.props.label} className="py-2 px-4 rounded-md border-2 border-icon" />
-      </div>
-    )
-  }
-}
+const StyledTextField = React.forwardRef<HTMLInputElement, StyledTextFieldProps>(({ title, label }, ref) => {
+  return (
+    <div className="flex flex-col">
+      <label className="block mb-2 text-subtitle font-bold" htmlFor={label}>{title}</label>
+      <input ref={ref} type="text" id={label} name={label} className="py-2 px-4 rounded-md border-2 border-icon" />
+    </div>
+  )
+})
+
+export default StyledTextField
