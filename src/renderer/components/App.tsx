890d0f0ed5d0c1a29a4bb54696aea9f7a35dd9b9
woojiahao <woojiahao1234@gmail.com>
2021-11-28 01:33:47 +0800

    Add padding right for app

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { TagFeeds } from "../../main/database/mappers/FeedMapper";
 import { getTagFeeds } from "../ipcInvoker";
+import AddFeedForm from "./AddFeedForm";
 import ArticleList from "./ArticleList/ArticleList";
 import ArticleViewer from "./ArticleViewer";
 import Home from "./Home";
@@ -20,12 +21,13 @@ const App = () => {
   }, [])
 
   return (
-    <div className="grid grid-cols-5 gap-12 overflow-y-hidden h-screen">
+    <div className="grid grid-cols-5 gap-10 overflow-y-hidden h-screen">
       <NavigationBar tagFeeds={tagFeeds} />
 
       {/* TODO: Add scroll history */}
       <Routes>
-        <Route path="/" element={<Home layout="col-span-4" />} />
+        <Route path="/" element={<Home layout="col-span-4 pr-10" />} />
+        <Route path="/add" element={<AddFeedForm layout="col-span-4 pr-16" />} />
         <Route path="/feed/:id" element={<ArticleList layout="col-span-3" />} />
         <Route path="/article/:id" element={<ArticleViewer layout="col-span-3" />} />
       </Routes>
